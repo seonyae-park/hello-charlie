1fad5bc223987aba352ae77901c4727e5be8dc57
ho-ho <46485664+seonyae-park@users.noreply.github.com>
2024-07-24 16:55:41 +0900

    feat: set up the environment for nextjs (#9)
    Next.js 기반의 프로젝트를 구성하고,
    - ui, font 를 위한 global 폴더를 추가합니다.
    - (아직 구조화되기 전이지만) 기본적으로 사용할 페이지를 위한 구조를 추가합니다.
      -  `/login`, `/dashboard`, `/calendar`, `/settings`, `/user`

---
+++ src/app/calendar/layout.tsx
@@ -0,0 +1,5 @@
+function Calendar({ children }: Readonly<{ children: React.ReactNode }>) {
+  return <div>{children}</div>
+}
+
+export default Calendar
---
+++ src/app/settings/page.tsx
@@ -0,0 +1,10 @@
+function Page() {
+  return (
+    <div>
+      <h1>Settings</h1>
+      <p>Settings page content</p>
+    </div>
+  )
+}
+
+export default Page
