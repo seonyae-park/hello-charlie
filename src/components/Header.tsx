6d536987e643dcd63e15ac1e1990f480782593eb
ho-ho <46485664+seonyae-park@users.noreply.github.com>
2024-08-05 18:18:44 +0900

    chore: set up the action for pr validation (#31)
    ## Problems :mag:
    - PR 마다 검증되지 않고 있어, 검증되지 않은 PR 이 main 브랜치에 적용되고 있습니다.
    
    ## Changes :memo:
    - PR 이 유효한지 검증하기 위한 몇가지 테스트를 액션으로 추가합니다.
    - PR 이 생성/갱신되거나 main 브랜치로 push 될 때마다 수행합니다.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,16 @@
 import { Box } from "@mui/material"
 import LogoIcon from "@assets/Logo.svg"
-import { getServerSession } from "next-auth"
+// import { getServerSession } from "next-auth"
 import LoginButton from "./LogInButton"
 import Profile from "./Profile"
 
 async function Header() {
-  const session = await getServerSession()
-  const validSession = session?.user
+  // const session = await getServerSession()
+  const validSession = true
   return (
     <Box className="f-row-between-center-4 p-12">
       <LogoIcon width="32" height="32" />
-      {validSession ? (
-        <LoginButton />
-      ) : (
-        <Profile imageUrl={session?.user?.image ?? ""} />
-      )}
+      {validSession ? <LoginButton /> : <Profile imageUrl={""} />}
     </Box>
   )
 }
--- src/components/LogInButton.tsx
+++ src/components/LogInButton.tsx
@@ -1,20 +1,20 @@
 "use client"
 
 import { Button } from "@mui/material"
-import { signIn, signOut, useSession } from "next-auth/react"
+// import { signIn, signOut, useSession } from "next-auth/react"
 import Link from "next/link"
 
 function LoginButton() {
-  const { data } = useSession()
+  // const { data } = useSession()
 
   const onClick = async (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault()
-    data ? await signOut() : await signIn("kakao")
+    // data ? await signOut() : await signIn("kakao")
   }
 
   return (
     <div>
-      {data?.user && <Button>Avatar</Button>}
+      <Button>Avatar</Button>
       <Link href="#" onClick={onClick}>
         SignIn
       </Link>
